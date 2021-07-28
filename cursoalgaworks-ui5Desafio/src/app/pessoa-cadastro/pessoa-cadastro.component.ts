4fd0e03a51cb29c3d6e1a54206c70090e60a4e58
joaolassi <jmlassi@outlook.com>
2021-07-28 17:47:02 -0300

    Desafio criando componentes 'pessoas-grid' e 'lancamentos-grid'

---
+++ cursoalgaworks-ui5Desafio/src/app/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-cadastro',
+  templateUrl: './pessoa-cadastro.component.html',
+  styleUrls: ['./pessoa-cadastro.component.css']
+})
+export class PessoaCadastroComponent implements OnInit {
+
+  numero?: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
