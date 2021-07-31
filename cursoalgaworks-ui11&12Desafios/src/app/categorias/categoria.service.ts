e138543e677be77d610c0a1b38337ae884b8b7f9
joaolassi <jmlassi@outlook.com>
2021-07-31 17:04:00 -0300

    Desafio atualizar status da pessoa e listar categorias

---
+++ cursoalgaworks-ui11&12Desafios/src/app/categorias/categoria.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl = 'http://localhost:8080/categorias';
+
+
+  constructor(private http: HttpClient) { }
+
+  listarTodas(): Promise<void> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==')
+
+    return this.http.get(`${this.categoriasUrl}`, { headers })
+      .toPromise()
+      .then();
+  }
+}
