8e4d44f42159982e86593ef3c92bf2d5592c509c
joaolassi <jmlassi@outlook.com>
2021-08-02 13:22:39 -0300

    Desafio cadastro de pessoas

---
+++ cursoalgaworks-ui14Desafio/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    LancamentosModule,
+    PessoasModule,
+    CoreModule,
+
+    AppRoutingModule
+  ],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cursoalgaworks-ui14Desafio/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,72 @@
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+import { MessageService } from 'primeng/api';
+
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { LancamentoService } from 'src/app/lancamentos/lancamento.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { Lancamento } from 'src/app/core/model';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ];
+
+  categorias = [];
+
+  pessoas = [];
+
+  lancamento = new Lancamento();
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private pessoasService: PessoaService,
+    private lancamentoService: LancamentoService,
+    private messageService: MessageService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit(): void {
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+      .then(categorias => {
+        // ".map" percorre todos os elementos e chama uma função pra cara elemento
+        this.categorias = categorias
+          .map((c: any) => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  salvar(form: NgForm) {
+    return this.lancamentoService.adicionar(this.lancamento)
+      .then(() => {
+        this.messageService.add({ severity: 'success', detail: 'Lançamento adicionado com sucesso!' });
+        form.reset();
+        this.lancamento = new Lancamento();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarPessoas() {
+    return this.pessoasService.listarTodas()
+      .then(pessoas => {
+        this.pessoas = pessoas
+          .map((p: any) => ({ label: p.nome, value: p.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+}
